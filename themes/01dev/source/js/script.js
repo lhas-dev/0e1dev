4e59c85aa0d8c98cf2507382e54e877409080da5
lhas <luizhrqas@gmail.com>
2018-09-05 02:42:51 -0300

    tried to fix the highlight stylesheet (without success =/)

--- themes/01dev/source/js/script.js
+++ themes/01dev/source/js/script.js
@@ -1,20 +1,22 @@
-const macy = Macy.init({
-    container: '#masonry',
-    margin: 30,
-    columns: 4,
-    breakAt: {
-        460: {
-            columns: 1,
-        },
-        860: {
-            columns: 2,
-        },
-        1100: {
-            columns: 3,
-        },
-    }
-});
+if(document.querySelectorAll('#masonry').length > 0) {
+    const macy = Macy.init({
+        container: '#masonry',
+        margin: 30,
+        columns: 4,
+        breakAt: {
+            460: {
+                columns: 1,
+            },
+            860: {
+                columns: 2,
+            },
+            1100: {
+                columns: 3,
+            },
+        }
+    });
 
-window.onload = function() {
-    macy.recalculate(true);
+    window.onload = function() {
+        macy.recalculate(true);
+    }
 }
